82a5504563e029f59d8c726fd8d6f55d34728093
TeaTimeChimp <58689005+TeaTimeChimp@users.noreply.github.com>
2025-08-12 14:26:12 +0100

    Fix tanh backward derivative

--- KoTanh.cpp
+++ KoTanh.cpp
@@ -9,9 +9,9 @@ NDArray KoTanh::Forward(const NDArrays& inputs)
 
 NDArrays KoTanh::Backward(const NDArray& gradient,const NDArrays& inputs)
 {
-	return
-	{
-		// Differental of tanh(x) WRT x is 1-tanh(x)^2, multiply this by the gradient and backprop.
-		(gradient.Ones()-(inputs[0]*inputs[0]))*gradient
-	};
+        return
+        {
+                // Differential of tanh(x) WRT x is 1 - tanh(x)^2; multiply this by the gradient and backprop.
+                (gradient.Ones()-(inputs[0].Tanh()*inputs[0].Tanh()))*gradient
+        };
 }
